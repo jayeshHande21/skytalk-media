f5b5dddc38b6738de1dc282364074fd880ad4ced
Jayu <jayeshhande00@gmail.com>
2023-07-22 14:49:31 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -8,33 +8,10 @@ import {
 } from "react-router-dom";
 import { useContext } from "react";
 
-import { Home } from "./Pages/Home";
-import { Header } from "./Components/Header";
-import { Explore } from "./Pages/Explore";
-import { Liked } from "./Pages/Liked";
-import { BookMarked } from "./Pages/BookMarked";
-import { UserProfile } from "./Pages/UserProfile";
-// import { MediaContext } from "../context/socialMediaContext";
-import { UserSuggesationData } from "./Pages/UserSuggestionData";
-import OIP from "./backend/Images/OIP.jpg";
-import { Login } from "../src/Pages/Authentication/Login";
-import { SignUp } from "./Pages/Authentication/SignUp";
-
 export default function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Explore" element={<Explore />} />
-        <Route path="/Liked" element={<Liked />} />
-        <Route path="/BookMarked" element={<BookMarked />} />
-        <Route path="/UserProfile" element={<UserProfile />} />
-        <Route path="/UserSuggesationData" element={<UserSuggesationData />} />
-      </Routes>
-
-      {/* <ToastContainer /> */}
+      <h1>Jayehs is Here</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { MediaContext, MediaProvider } from "./Context/socialMediaContext";
-
+// import { BrowserRouter as Router } from "react-router-dom";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-export { MediaContext };
 
 makeServer();
 root.render(
   <StrictMode>
-    <Router>
-      <MediaProvider>
-        <App />
-      </MediaProvider>
-    </Router>
+    <App />
   </StrictMode>
 );
